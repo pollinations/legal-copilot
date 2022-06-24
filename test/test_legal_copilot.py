87619c12c25f7e89b615578cc8fa3de59e5db31f
Niels Warncke <niels.warncke@gmail.com>
2022-06-24 14:52:41 +0200

    test added

--- test/test_legal_copilot.py
+++ test/test_legal_copilot.py
@@ -1,11 +1,36 @@
-from legal_copilot.legal_copilot import fib
+import requests
 
 
-def test_fib() -> None:
-    assert fib(0) == 0
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(3) == 2
-    assert fib(4) == 3
-    assert fib(5) == 5
-    assert fib(10) == 55
+def test_localhost() -> None:
+    response = requests.post(
+        "http://localhost:5000/legal/",
+        json={
+            "original": """In order to avoid the insolvency of the
+Company within the meaning of section.
+17 para. 2 of the German Insolvency
+Statute (\"InsO\") and over-indebtedness
+of the Company within the meaning of
+section 19 para. 2 InsO the Lender
+subordinates towards the Company all
+current and future claims arising from
+this Agreement including repayment and
+interest (\"Subordinated Claims\")
+according to section 39 para. 2 German
+Insolvency Code to all present and future
+claims of present and future creditors of
+the Company, except for the claims of the
+other lenders under the other Agreements
+of the Convertible Loan Financing
+Round, in particular to claims as set forth
+in section 39 para. 1 n. 1 to 5 InsO. The
+claims of all lenders of the Convertible
+Loan Financing Round shall rank
+equally."""
+        },
+    )
+    assert response.status_code == 200
+    print(response.json()["simple"])
+
+
+if __name__ == "__main__":
+    test_localhost()
