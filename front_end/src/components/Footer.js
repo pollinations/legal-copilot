2e2ca8a5282e5abeab37a50fd0a2e8b0da57daf3
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-08-12 13:01:29 +0200

    footer notes

--- front_end/src/components/Footer.js
+++ front_end/src/components/Footer.js
@@ -6,13 +6,26 @@ const Footer = () => <TextContainer>
         Made with <a target="_blank" href='https://pollinations.ai' rel="noreferrer">
         Pollinations</a>
     </Typography>
+    <Typography variant='h5'>
+       <i> This is not legal advice. The Legal Translator is not a lawyer, always read your contracts thoroughly.</i>
+    </Typography>
 </TextContainer>
 
 const TextContainer = styled.div`
-text-align: center;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
+margin: 0 2em;
+
 margin-top: 1.5em;
 h6 {
     font-size: 1.2em;
+    margin: 0.5em 0;
+}
+h5 {
+    font-size: 0.9em;
+    margin: 0.5em 0;
 }
 `
 
